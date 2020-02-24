ca6f9f7956e6670302d2f7680daf7451abb21ece
Igor Maznitsa <rrg4400@gmail.com>
2020-02-24 22:48:24 +0200

    initial

---
+++ forph-engine/src/main/java/com/igormaznitsa/forph/ForphContext.java
@@ -0,0 +1,4 @@
+package com.igormaznitsa.forph;
+
+public class ForphContext {
+}
--- forph-engine/src/main/java/com/igormaznitsa/forph/ForphEngine.java
+++ forph-engine/src/main/java/com/igormaznitsa/forph/ForphEngine.java
@@ -1,7 +1,33 @@
 package com.igormaznitsa.forph;
 
+import java.io.IOException;
+import java.io.Reader;
+
 public class ForphEngine {
-  public ForphEngine(){
+  public ForphEngine() {
 
   }
+
+  public void interpret(final ForphContext context, final Reader reader) throws IOException {
+    while (true) {
+      final String nextWord = this.next(reader);
+      if (nextWord.isEmpty()) {
+        break;
+      }
+    }
+  }
+
+  private String next(final Reader reader) throws IOException {
+    final StringBuilder buffer = new StringBuilder();
+    while (true) {
+      final int nextChar = reader.read();
+      if (nextChar < 0
+          || Character.isWhitespace(nextChar)
+          || Character.isISOControl(nextChar)) {
+        break;
+      }
+      buffer.append((char) nextChar);
+    }
+  }
+
 }
